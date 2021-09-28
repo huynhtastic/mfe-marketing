21938b5b0ba6a200a8912b892c286dc6d9927c82
Richard <richard@getcerebral.com>
2021-09-27 21:39:31 -0500

    change import

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
