32ee919da310822a1f3d310790967af51e4f60ac
Richard <richard@getcerebral.com>
2021-09-29 18:40:57 -0500

    default to browserhistory in dev mode

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // mount function to start app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
   onNavigate && history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
@@ -24,7 +24,7 @@ const mount = (el, { onNavigate }) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
-  devRoot && mount(devRoot, {});
+  devRoot && mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 // case 2: assume we are in container, export mount
