7aba31a2007b8378a3a74eedb0a38deab095b743
Richard <richard@getcerebral.com>
2021-10-11 21:05:21 -0500

    pass initial path to marketing app

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,8 +4,12 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // mount function to start app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
   onNavigate && history.listen(onNavigate);
 
   ReactDOM.render(<App history={history} />, el);
